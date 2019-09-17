090137adf4965f259f7c97028aa14700510766ce
David Gibbons <dgibbons999@gmail.com>
2019-09-17 09:26:19 +0100

    Initial commit

---
+++ apps/frontend/js/script.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+  el: '#app',
+  delimiters: ["[[","]]"],
+  data() {
+    return {
+      quoteText: 'dsasd',
+      quoteAuthor: '',
+    };
+  },
+  created() {
+    this.getQuote();
+  },
+  methods: {
+    getQuote() {
+      const url = '/random-quote';
+      fetch(url)
+        .then(response => response.json())
+        .then(json => {
+          this.quoteText = json.text;
+          this.quoteAuthor = json.author;
+        });
+    },
+  },
+})
+
