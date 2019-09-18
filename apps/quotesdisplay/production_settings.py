06ee5e8aad729d380232cf115dfb5cb141a6b934
David Gibbons <dgibbons@eircom.net>
2019-09-18 16:42:53 +0100

    Add prod settings

---
+++ apps/quotesdisplay/production_settings.py
@@ -0,0 +1,7 @@
+from .settings import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['davgibbs.pythonanywhere.com', 'davgibbs2.pythonanywhere.com']
+
